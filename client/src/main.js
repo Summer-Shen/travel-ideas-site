2c601cc79f7fa78fb13222a216cb74bb17fd4aa1
Summer-Shen <1563407190@qq.com>
2023-08-03 00:55:13 +0800

    add tdesign-vue-next dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import TDesign from 'tdesign-vue-next'
 
 import App from './App.vue'
 import router from './router'
+
+// imports global style variables from tdesign
+import 'tdesign-vue-next/es/style/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(TDesign)
 
 app.mount('#app')
