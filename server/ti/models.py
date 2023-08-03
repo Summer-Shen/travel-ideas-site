0b9c49438229187e7e0e4ea70bbbe42b9fdeef00
Summer-Shen <1563407190@qq.com>
2023-08-03 23:39:34 +0800

    add `__str__` method overrides in models.py

--- server/ti/models.py
+++ server/ti/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
     password = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Idea(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -17,11 +20,17 @@ class Idea(models.Model):
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
 
 
 class Tag(models.Model):
     idea_id = models.ForeignKey(Idea, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):
@@ -29,3 +38,6 @@ class Comment(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.content
