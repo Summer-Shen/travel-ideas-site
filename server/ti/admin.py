7fad7abefde21f39cc354db01c38faccb19c0b8e
Summer-Shen <1563407190@qq.com>
2023-08-03 22:08:26 +0800

    create `UserAdmin` in admin.py

--- server/ti/admin.py
+++ server/ti/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from ti.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+
+
 # Register your models here.
+admin.site.register(User, UserAdmin)
