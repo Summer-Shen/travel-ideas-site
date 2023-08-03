294188ff2b0d915e03dccef886bb8a812a6c67ea
Summer-Shen <1563407190@qq.com>
2023-08-03 22:44:32 +0800

    create `IdeaAdmin`, `TagAdmin`, `CommentAdmin` in admin.py

--- server/ti/admin.py
+++ server/ti/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 
-from ti.models import User
+from ti.models import User, Idea, Tag, Comment
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'created_at')
+    list_display = ("name", "email", "created_at")
+
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "title", "destination", "start_date", "end_date", "created_at", "modified_at")
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("idea_id", "name")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("idea_id", "user_id", "content", "created_at")
 
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Idea, IdeaAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment, CommentAdmin)
