6025e4be30edc71877c04af1f4ba53f8bb1f9133
Summer-Shen <1563407190@qq.com>
2023-08-03 22:31:09 +0800

    create models, migrations for ideas, tags & comments; apply migrations to db

--- server/ti/models.py
+++ server/ti/models.py
@@ -7,3 +7,25 @@ class User(models.Model):
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=64)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Idea(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    idea_id = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+
+class Comment(models.Model):
+    idea_id = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
