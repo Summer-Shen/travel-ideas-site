0d84504e38b8571eba0f6da61b5ca566e10ee17d
Summer-Shen <1563407190@qq.com>
2023-08-04 18:39:38 +0800

    add validation for email addr in `register()`

--- server/ti/views.py
+++ server/ti/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.hashers import make_password, check_password
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from django.db.models import Count
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -14,6 +16,10 @@ from .serializers import IdeaSearchItemSerializer, UserSerializer
 def register(request):
     name = request.POST["name"]
     email = request.POST["email"]
+    try:
+        validate_email(email)
+    except ValidationError:
+        return Response("Please input valid email address.", status=status.HTTP_400_BAD_REQUEST)
     password = request.POST["password"]
     hashed_password = make_password(password)
     user = User(name=name, email=email, password=hashed_password)
@@ -32,7 +38,7 @@ def login(request):
         serializer = UserSerializer(user, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
-        return Response("Incorrect email or password, please try again.", status=status.HTTP_200_OK)
+        return Response("Incorrect email or password, please try again.", status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
