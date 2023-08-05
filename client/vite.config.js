72c3a0d5850aecf2f13f73968a7747e5d456ba0a
Summer-Shen <1563407190@qq.com>
2023-08-05 14:33:07 +0800

    config server port, proxy options for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,13 +6,20 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: '3000',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/api/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
